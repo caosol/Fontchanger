093348ce729a9247385052946c20cf4706d847d9
John Fawkes <33303104+JohnFawkes@users.noreply.github.com>
2020-02-16 05:12:23 -0500

    add osm0sis ndk bb support
    Signed-off-by: John Fawkes <33303104+JohnFawkes@users.noreply.github.com>

--- customize.sh
+++ customize.sh
@@ -6,7 +6,11 @@ set -euo pipefail
 trap 'exxit $?' EXIT
 unzip -o "$ZIPFILE" 'module.prop' -d $MODPATH 2>&1
 SKIPUNZIP=1
-WGET="/data/adb/magisk/busybox wget"
+if [ -d /data/adb/modules/busybox-ndk ]; then
+  WGET="/data/adb/modules/busybox-ndk/system/xbin/busybox wget"
+else 
+  WGET="/sbin/.magisk/busybox/busybox wget"
+fi
 ui_print " - Downloading Installer Script"
 $WGET -O $TMPDIR/installer.sh https://github.com/johnfawkes/fontchanger-scripts/raw/master/installer.sh 2>/dev/null
 . $TMPDIR/installer.sh
